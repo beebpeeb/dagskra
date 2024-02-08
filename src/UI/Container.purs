d452e7020a1fc7e551fda659ed688908c9f4b005
Paul Burt <paul.burt@bbc.co.uk>
2024-02-08 22:22:27 +0000

    Updated deps

--- src/UI/Container.purs
+++ src/UI/Container.purs
@@ -2,9 +2,10 @@ module TV.UI.Container where
 
 import Prelude
 
+import Control.Monad.State.Class (modify_)
 import Data.Maybe (Maybe(..))
 import Effect.Aff.Class (class MonadAff, liftAff)
-import Halogen (Component, defaultEval, mkComponent, mkEval, modify_)
+import Halogen (Component, defaultEval, mkComponent, mkEval)
 import Halogen.HTML (html_)
 import Network.RemoteData (RemoteData(..))
 
@@ -27,10 +28,9 @@ component =
               }
     }
   where
-  handleAction action = case action of
-    FetchSchedule -> do
-      response <- liftAff fetchSchedule
-      modify_ _ { response = response }
+  handleAction FetchSchedule = do
+    response <- liftAff fetchSchedule
+    modify_ _ { response = response }
 
   initialState = const { response: Loading }
 
